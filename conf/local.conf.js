432ce8086a0a88b54149be73b61585644b2905c0
Harish Ved <ved.harish3@gmail.com>
2016-09-28 13:12:14 +0530

    test if local is connected

--- conf/local.conf.js
+++ conf/local.conf.js
@@ -1,5 +1,3 @@
-var browserstack = require('browserstack-local');
-
 exports.config = {
   'specs': [ '../specs/local.js' ],
   'seleniumAddress': 'http://hub-cloud.browserstack.com/wd/hub',
@@ -20,19 +18,12 @@ exports.config = {
 
   // Code to start browserstack local before start of test
   beforeLaunch: function(){
-    console.log("Connecting local");
     return new Promise(function(resolve, reject){
-      exports.bs_local = new browserstack.Local();
-      exports.bs_local.start({'key': exports.config.capabilities['browserstack.key'] }, function(error) {
-        if (error) return reject(error);
-        console.log('Connected. Now testing...');
-
-        resolve();
-      });
-    });
+      console.log('Now testing...');
+      resolve();
+    })
   },
 
-  // Code to stop browserstack local after end of test
   afterLaunch: function(){
     return new Promise(function(resolve, reject){
       exports.bs_local.stop(resolve);
